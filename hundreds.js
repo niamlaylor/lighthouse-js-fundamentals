98c6672545ff5b5305b90b35b1f6471d0cb9be8b
Liam Naylor <liam@liamnaylor.com>
2022-08-21 11:32:06 -0700

    how many hundreds exercise

---
+++ hundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds(number) {
+  let dividedNumber = number / 100
+  if (dividedNumber >= 1) {
+    return Math.floor(dividedNumber);
+  }
+  else {
+    return Math.floor(dividedNumber);
+  }
+} console.log(howManyHundreds(99));
