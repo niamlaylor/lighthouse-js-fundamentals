422e934bfc2e16b5775a93ecc4a78d66248437fc
Liam Naylor <liam@liamnaylor.com>
2022-08-21 11:34:55 -0700

    Refactored to simplify

--- hundreds.js
+++ hundreds.js
@@ -1,9 +1,5 @@
 function howManyHundreds(number) {
   let dividedNumber = number / 100
-  if (dividedNumber >= 1) {
-    return Math.floor(dividedNumber);
-  }
-  else {
-    return Math.floor(dividedNumber);
-  }
-} console.log(howManyHundreds(99));
+  return Math.floor(dividedNumber);
+} 
+console.log(howManyHundreds(2039));
