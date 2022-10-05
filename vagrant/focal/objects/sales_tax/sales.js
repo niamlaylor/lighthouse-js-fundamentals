6d504f3251b4506c8f4dabe3d194f729d569ef96
Liam Naylor <liam@liamnaylor.com>
2022-10-05 15:30:24 -0700

    Last commit

--- vagrant/focal/objects/sales_tax/sales.js
+++ vagrant/focal/objects/sales_tax/sales.js
@@ -22,26 +22,26 @@ const companySalesData = [
   }
 ];
 
-const sumSalesAndTax = function(salesArray, taxPercent) {
+const sumSalesAndTax = function(salesArray, taxPercent) { //separate function outside of my main function to calculate total sales and the appropriate tax.
   let totalSales = 0;
   for (let number of salesArray) {
-    totalSales += number;
+    totalSales += number; // Simple total for loop
   }
-  return {totalSales, totalTaxes: totalSales * taxPercent};
+  return {totalSales, totalTaxes: totalSales * taxPercent}; // This returns a new property of totalSales that's equal to itself and totalTaxes which is equal to totalSales times the appropriate percentage
 };
 
 const calculateSalesTax = function(salesData, taxRates) {
-  const results = {};
+  const results = {}; //Defining an empty results object to pass the results into
 
   for (let company of salesData) {
-    const {totalSales, totalTaxes} = sumSalesAndTax(company.sales, taxRates[company.province])
-    if (results[company.name]) {
+    const {totalSales, totalTaxes} = sumSalesAndTax(company.sales, taxRates[company.province]) // This is using a new concept to store the outputs from the sum function above and make them able to be referenced below
+    if (results[company.name]) { //This is adding logic on what to do if there's an existing property for the company (i.e. you add the new sum to the existing value)
       results[company.name].totalSales += totalSales;
       results[company.name].totalTaxes += totalTaxes;
     }
-    else {
+    else { // This is the logic for what to do if the company isn't found as a property (i.e. it defines a new object for the new company).
       results[company.name] = {
-        totalSales,
+        totalSales, // This is short form instead of writing "totalSales: totalSales"
         totalTaxes
       };
     }
