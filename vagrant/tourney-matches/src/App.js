5db823024b744888e73ee35ea04d0c7ab05258f1
Liam Naylor <liam@liamnaylor.com>
2023-01-19 14:05:11 -0800

    Add components + helpers + mock data

---
+++ vagrant/tourney-matches/src/App.js
@@ -0,0 +1,15 @@
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tourney Matches{" "} <span>Where Coding and Tournaments found their Match!</span></h1>
+      <PlayerList />
+      <MatchList />
+    </div>
+  );
+};
+
+export default App;
